bd3a11c5d26b92a94d6a27ba3d264afa5127f1b4
Dmitry Shloma <dmitry-shloma@mail.ru>
2019-06-03 01:05:29 +0600

    optimize build script

--- build.bat
+++ build.bat
@@ -12,30 +12,27 @@ set PROGRAMMER=usbasp
 set MCU=atmega8
 
 cd /d "%CURRENT_DIR_PATH%"
-
 rmdir /s /q "%OUT_DIR_NAME%" 2>nul
 mkdir "%OUT_PROJECT_PATH%"
 
-cd /d "src/libcharlcd"
-%COMPILER% --std=c99 -mmcu=%MCU% -g -Os -c ./charlcd.c -o libcharlcd.o
-move /y libcharlcd.o "%CURRENT_DIR_PATH%/src/char-lcd" >nul
+cd /d "%CURRENT_DIR_PATH%\src\libcharlcd"
+%COMPILER% --std=c99 -mmcu=%MCU% -g -Os -c ./charlcd.c -o "%CURRENT_DIR_PATH%"\src\char-lcd\libcharlcd.o
 
-cd /d "%CURRENT_DIR_PATH%/src/char-lcd"
-%COMPILER% --std=c99 -g -mmcu=%MCU% libcharlcd.o ./main.c -o char-lcd.elf
+cd /d "%CURRENT_DIR_PATH%\src\char-lcd"
+%COMPILER% --std=c99 -g -mmcu=%MCU% libcharlcd.o ./main.c -o "%CURRENT_DIR_PATH%\%OUT_PROJECT_PATH%"\char-lcd.elf
 del /q libcharlcd.o >nul
 
+cd /d "%CURRENT_DIR_PATH%\%OUT_PROJECT_PATH%"
 avr-objcopy -j .text -j .data -O ihex char-lcd.elf char-lcd.hex
 del /q char-lcd.elf >nul
-move /y char-lcd.hex "%CURRENT_DIR_PATH%\%OUT_PROJECT_PATH%" >nul
 
 :choice
-set /P c=flash[Y/N]?
+set /P c="flash[Y/N]: "
 if /I "%c%" EQU "Y" goto :flash
 if /I "%c%" EQU "N" goto :no_flash
 goto :choice
 
 :flash
-cd /d "%CURRENT_DIR_PATH%\%OUT_PROJECT_PATH%"
 avrdude -c %PROGRAMMER% -p %MCU% -U flash:w:char-lcd.hex:i
 pause
 exit
